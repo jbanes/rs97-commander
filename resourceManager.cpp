4db42d68a5ea67764978aa456fb661b402bd489b
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-01-19 14:55:50 +0000

    Switch font to NotoSansCJK-Regular

--- resourceManager.cpp
+++ resourceManager.cpp
@@ -24,7 +24,7 @@ CResourceManager::CResourceManager(void) :
     m_surfaces[T_SURFACE_CURSOR1] = SDL_utils::createImage(159, LINE_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_CURSOR_1));
     m_surfaces[T_SURFACE_CURSOR2] = SDL_utils::createImage(159, LINE_HEIGHT * PPU_Y, SDL_MapRGB(Globals::g_screen->format, COLOR_CURSOR_2));
     // Load font
-    m_font = SDL_utils::loadFont("res/wy_scorpio.ttf", 8);
+    m_font = SDL_utils::loadFont("res/NotoSansCJK-Regular.ttc", 8);
 }
 
 void CResourceManager::sdlCleanup(void)
